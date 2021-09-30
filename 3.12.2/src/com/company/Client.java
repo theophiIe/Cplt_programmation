ab0a236dd2e25a0d3b5f151594ea558e2de5d774
theophiIe <theomolinatti@gmail.com>
2021-09-30 21:35:43 +0200

    Creation des templates des méthodes pour les deux classes

--- 3.12.2/src/com/company/Client.java
+++ 3.12.2/src/com/company/Client.java
@@ -1,5 +1,21 @@
 package com.company;
 
 public class Client {
+    private String nom;
 
+    public Client(String nom) {
+        this.nom = nom;
+    }
+
+    public boolean seConnecter(Serveur serveur) {
+        return true;
+    }
+
+    public void envoyer(String message) {
+
+    }
+
+    public void recevoir(String message) {
+
+    }
 }
--- 3.12.2/src/com/company/Serveur.java
+++ 3.12.2/src/com/company/Serveur.java
@@ -2,4 +2,15 @@ package com.company;
 
 public class Serveur {
 
+    public Serveur() {
+
+    }
+
+    public boolean connecter(Client client) {
+        return true;
+    }
+
+    public void diffuser(String message) {
+
+    }
 }
