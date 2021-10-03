bac55ab7005d8a7c6c0b448b798997cdb1dbc514
theophiIe <theomolinatti@gmail.com>
2021-10-03 16:11:40 +0200

    Implémentation des fonctions add

--- 4.7.3/src/com/company/Arborescence.java
+++ 4.7.3/src/com/company/Arborescence.java
@@ -6,11 +6,16 @@ import java.util.ArrayList;
 
 public class Arborescence {
     private HashMap<Dossier, List<Dossier>> arborescence;
+    private Dossier root = new Dossier("root");
 
     public Arborescence() {
         this.arborescence = new HashMap<Dossier, List<Dossier>>();
         List<Dossier> liste = new ArrayList<Dossier>();
-        this.arborescence.put(new Dossier("root"), liste);
+        this.arborescence.put(root, liste);
+    }
+
+    public void add(Dossier ajout_dossier) {
+        this.arborescence.get(this.root).add(ajout_dossier);
     }
 
     public void add(Dossier dossier, Dossier ajout_dossier) {
@@ -23,6 +28,10 @@ public class Arborescence {
                 List<Dossier> liste = new ArrayList<Dossier>();
                 liste.add(ajout_dossier);
                 this.arborescence.replace(dossier, liste);
+            }
+
+            else {
+                this.arborescence.get(dossier).add(ajout_dossier);
             }
         }
     }
--- 4.7.3/src/com/company/Main.java
+++ 4.7.3/src/com/company/Main.java
@@ -2,6 +2,29 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("TD1 : 4.7.3");
 
+        Arborescence arbo = new Arborescence();
+
+        Dossier d1 = new Dossier("D1");
+        Dossier d2 = new Dossier("D2");
+        Dossier d3 = new Dossier("D3");
+        Dossier d4 = new Dossier("D4");
+
+        Fichier f1 = new Fichier("F1", 10);
+        Fichier f2 = new Fichier("F2", 5);
+        Fichier f3 = new Fichier("F3", 30);
+        Fichier f4 = new Fichier("F4", 100);
+
+        d1.add(f1);
+        d2.add(f2);
+        d3.add(f3);
+        d3.add(f4);
+
+        arbo.add(d1);
+        arbo.add(d1, d2);
+        arbo.add(d4, d3);
+
+        d1.afficheContenu();
     }
 }
